b405c60e792bd129e86e78de3f1ba13126e18f98
Harvey Rendell <harvey.rendell@pushpay.com>
2018-12-03 13:09:58 +1300

    Day 1 - part 2

--- day_1/frequency.py
+++ day_1/frequency.py
@@ -1,4 +1,26 @@
 # https://adventofcode.com/2018/day/1
 
-with open('input.txt', 'r') as input:
-    print(sum([int(line) for line in input.readlines()]))
+INPUT_FILE = 'input.txt'
+
+# part 1
+with open(INPUT_FILE, 'r') as input:
+    answer = sum([int(line) for line in input.readlines()])
+    print(f'part 1 answer: {answer}')
+
+
+
+# part 2
+def find_repeated_frequency(lines):
+    total = 0
+    seen = {0: True}
+
+    while True:
+        for value in lines:
+            total += value
+            if total in seen: return total
+            seen[total] = True
+
+with open(INPUT_FILE, 'r') as input:
+    lines = [int(line) for line in input.readlines()]
+    answer = find_repeated_frequency(lines)
+    print(f'part 2 answer: {answer}')
